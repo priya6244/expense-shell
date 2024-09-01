5caa48573dd5705087eac6246019adcdfe0fb2d8
priya <haripriya.godishala12@gmail.com>
2024-09-01 16:19:11 +0530

    backend

--- frontend.sh
+++ frontend.sh
@@ -34,27 +34,27 @@ echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
 CHECK_ROOT
 
-dnf install nginx -y &>>$LOG_FILE
+dnf install nginx -y #&>>$LOG_FILE
 VALIDATE $? "Installing Nginx"
 
-systemctl enable nginx &>>$LOG_FILE
+systemctl enable nginx #&>>$LOG_FILE
 VALIDATE $? "Enable Nginx"
 
-systemctl start nginx &>>$LOG_FILE
+systemctl start nginx #&>>$LOG_FILE
 VALIDATE $? "Start Nginx"
 
-rm -rf /usr/share/nginx/html/* &>>$LOG_FILE
+rm -rf /usr/share/nginx/html/* #&>>$LOG_FILE
 VALIDATE $? "Removing default website"
 
-curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOG_FILE
+curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip #&>>$LOG_FILE
 VALIDATE $? "Downloding frontend code"
 
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip &>>$LOG_FILE
+unzip /tmp/frontend.zip
 VALIDATE $? "Extract frontend code"
 
 cp /home/ec2-user/expense-shell/expense.conf /etc/nginx/default.d/expense.conf
 VALIDATE $? "Copied expense conf"
 
-systemctl restart nginx &>>$LOG_FILE
+systemctl restart nginx #&>>$LOG_FILE
 VALIDATE $? "Restarted Nginx"
